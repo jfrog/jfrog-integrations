1f9255876a9cfab14b239d268e9addb368f54f7c
shivaramr <shivaramr@jfrog.com>
2018-06-29 17:07:44 -0700

    sonar integration

---
+++ sonarqube/artifactory/README.md
@@ -0,0 +1,148 @@
+# SonarQube Integration with Artifactory Build-Info #
+
+In your development environment, you might already be using SonarQube to scan your code to detect
+security vulnerabilities, code smells, coverage and bugs, most likely via  [Quality Gate Metrics][8a22582c].
+Now this integration allows the above information to be viewed in the context of a build( Artifactory build_info)
+
+  [8a22582c]: https://docs.sonarqube.org/display/SONAR/Quality+Gates "SonarQualityGates"
+
+
+
+This integration is also CI agnostic and can be used in many of the popular CI environments like Jenkins, Travis and CircleCI.
+
+The main integration component of interest is the supplied [artifactory-sonar.sh](https://github.com/jfrog/jfrog-integration/blob/master/sonarqube/artifactory/artifactory-sonar.sh). Full details are listed below.
+
+![Artifactory-SonarQube Integration](images/jfrogsonarintro.png)
+
+
+## Prerequisites for this integration ##
+
+1. Build machine already has all the dependencies installed (e.g curl, grep, sed, awk, jq )
+
+
+2. Build enviroment is configured to run with sonar analysis and quality gate meterics defined for the  project.
+
+3. Environment variables ARTIFACTORY_USER, ARTIFACTORY_APIKEY, ARTIFACTORY_URL and ARTIFACTORY_REPO have to be set in CI pipeline scripts like Jenkinsfile or saved in a local file for example in ~/jfrogsonar.env which can be sourced before invoking the script.
+
+
+## How to use the artifactory-sonar integration ##
+
+It is recommended to use the artifactory-sonar integration  script as a reference as pipelines and conventions vary from company to company. Artifactory-Sonar integration can be used in **WAIT**  (blocking) mode or **NOWAIT** (non-blocking) mode. Wait
+mode is the default mode.
+
+#### WAIT MODE ####
+
+In the wait mode, the script waits for the sonar analysis to complete, so the build-info has the complete quality gate status information. The information that is captured in the build_info includes the sonar compute engine task id referred to as the CeTaskId,  the status of quality gate analysis referred to as the taskStatus,the sonar analysis id, the overall quality gate status, and the metric key name and status of all the quality gate conditions. Based on the quality gate condition status, the build can be marked as failed.
+
+This mode is enabled when environment  variable WAIT_FOR_ANALYSIS is set to "true", which is also the default.
+
+Invoke the script artifactory-sonar.sh in the post-build phase of your CI pipeline.
+
+e.g in a typical Jenkinsfile
+
+```
+stage('Post Build') {
+
+    sh -c ". ~/jfrogsonar.env; ./artifactory-sonar.sh"
+
+}
+```
+
+If we do not want to fail the build, invoke the script with the argument "false".
+
+    ./artifactory-sonar.sh false
+
+
+
+#### NOWAIT MODE ####
+
+In the no-wait mode, the script does not wait for the analysis to complete. This mode is enabled when
+the environment variable WAIT_FOR_ANALYSIS is set to "false".
+
+To invoke in no wait mode, execute
+
+    export WAIT_FOR_ANALYSIS=false; ./artifactory-sonar.sh
+
+The information that is captured in the build_info includes CeTaskId and the taskStatus which will be PENDING. The CeTaskId can be used to get the analysis id and quality gate information as implemented in the function waitForAnalysisToComplete() in the
+artifactory-sonar.sh script.
+
+This information can be used in deciding whether to promote this build or not in the pipeline.
+
+
+
+## Integration Script ( artifactory-sonar.sh ) details ##
+
+When the Intgeration script artifactory-sonar.sh is invoked, it does the following :
+
+1. If the project build is integrated with sonar, then after every build, a file report-task.txt, referred to as "sonar report file" is produced by sonar. The location of this file varies. In case of maven build, will be in target/sonar directory. This file contains the Sonar Compute Engine Task Id referred to as the CeTaskId and the sonar server url as well  as the dashboard url and the project id. The script checks  if the sonar report file exists in the standard places and uses the sonar CeTaskID and the server url to check the status of the sonar task.
+
+2. Even though build completed and report-task.txt got published, the analysis may not be complete as it is an asynchronous process on the sonar server. So in order to get the analsyis id, the script waits for the analysis to complete by default.  The CeTaskId is used to query the server for status in a loop for this purpose. This beahvior can be changed by setting envonemnt variable WAIT_FOR_ANALYSIS to false which will make the script to not wait for anaylysis to complete.
+
+3. The script at this times exports two environment variables SONAR_CETASKID set to the CeTaskId and SONAR_TASKSTATUS set to PENDING if enviroment variable WAIT_FOR_ANALYSIS set to false. If WAIT_FOR_ANALYSIS is set, and we got analysis id, then the status will be SUCCESS. (Otherwise, it maybe FAILED or CANCELLED)
+
+4. If the status is success, analysis id is obtained and from the analysis id quality gate status is obtained and  quality gate conditions are unravelled and the following enviroment variables are set.
+
+    SONAR_ANALYSIS_ID,
+
+    SONAR_QGATESTATUS.
+
+    There will be an evironment variable for every metric in the Qualtiy Gate condtion with the value set to its status. For if there are five quality gate conditions, you will see the following enviroment varaibles being set.
+
+    SONAR_new_security_rating=ERROR;
+
+    SONAR_new_reliability_rating=OK;
+
+    SONAR_new_maintainability_rating=OK;
+
+    SONAR_coverage=ERROR;
+
+5. The script will install jfrog cli and connects to artifactory, collects all the environment variables and publishes to artifactory. The build info environment details can be viewed in the artifactory.
+
+
+![Artifactory Build Info](https://github.com/jfrog/jfrog-integrations/blob/master/sonarqube/artifactory/images/artifactory_buildinfo.png)
+
+6. If quality gate status has failed, the script will exit with status 1, so the build can be failed.
+
+7. Script execution options.
+
+    a. The script can be executed with WAIT_FOR_ANALYSIS=false. This is recommended to ensure builds complete quickly in cases where analysis time is very long. In this case, the script will not wait for analysis to complete, and just sets the task id and status which will be pending and the build info with these two variables will get publishde. Of course, if we do not wait for analysis to complete, so we will not have quality gate status and so we will not be able to fail the build even if enabled.
+
+    b. The script can be executed with argument false, which will not fail the build when quality gate status is ERROR.
+
+8. The curl commands in the script need to be modified based on the SonarQube authentication approach adopted.
+
+9. The artifacts to be uploaded will also require changes based on the project being built.
+
+## Sample Jenkinsfile ##
+
+The sample [Jenkinsfile](https://github.com/jfrog/jfrog-integrations/blob/master/sonarqube/artifactory/Jenkinsfile) shows how and where it can be invoked.
+
+## Test environment details ##
+
+1. SonarQube 7.2
+2. Artifactory 6.0
+3. Jenkins
+
+
+
+
+## Uses of this integration ##
+
+It is now possible to tie a build info with the quality gate status and track the progress
+of quality improvements with data recorded in artifactory even if sonarqube data for each build is not saved and available.
+
+It is also useful for other JFrog tools  to use the
+SonarQube quality data available in the build info and display it with other useful
+information.
+
+## Future Steps ##
+
+1. Client GUI products can use the quality gate information directly or obtain the details from the CeTaskID.
+2. We can utilize webhooks in SonarQube to indicate when analysis completes and inform listeners and the listener applications can then be configured to utilize the quality gate information.
+3. Direct integration with other JFrog products.
+
+## References ##
+
+1. JFrog Artifactory:  http://www.jfrog.com
+2. SonarQube : http://www.sonarsource.com
+3. Github: https://github.com/jfrog-integrations/artifactory
---
+++ sonarqube/artifactory/artifactory-sonar.sh
@@ -0,0 +1,280 @@
+#!/bin/bash
+
+# This script has to be invoked in the post-build phase
+# of the pipeline. e.g 
+#
+# stage ("Post-Build") {
+#     sh -c "export WAIT_FOR_ANALYSIS=true; ./artifactory-sonar.sh [true|false]"
+#      ..... 
+#  }
+# 
+#The pipeline environment is expected to set the following environment variables.
+#
+# 1. All CI specific default variables  
+# 
+# 2. ARTIFACTORY_URL, ARTIFACTORY_USER, ARTIFACTORY_APIKEY, ARTIFACTORY_REPO
+#
+# 3. WAIT_FOR_ANALYSIS to be set to "true" for synchronous operation, false
+#    if no need to wait for the analysis to complete. If false, 
+#    only SONAR_TASKSTATUS, SONAR_CETASKID environment variables will be
+#    uploaded to build_info. SONAR_CETASKID has to be used to get all the 
+#    other detais like ANALYSIS_ID and quality gate metrics and values
+#    default = true
+#
+# 4. Invoke the script with argument "true" if we want to fail the build based on quality gate status.
+#    Even if set to true, but quality gate information is not available
+#   (if WAIT_FOR_ANALYSIS is set to false) then build cannot be failed.
+#
+# Also ensure that dependent command 'jq' is already installed and PATH
+# environment variable configured to find jq, grep and awk.
+
+
+# 
+# Function waitForAnalysisToComplete
+#
+# Call this function after getting the taskid and server url from report-task.txt.
+#
+# This will use curl command to query the sonar server with the taskid to get the status
+# If the status is PENDING or IN_PROGRESS, it will loop until it gets a SUCCESS
+# or FAILED or CANCELLED status.
+#
+# Then it will get the analysisID from server and gets quality gate status.
+# Using jq, it will get all the conditions and status and exports all them as environment
+# variables.
+
+
+waitForAnalysisToComplete() {
+
+	while [ "$taskstatus" == "PENDING" -o "$taskstatus" == "IN_PROGRESS" ];
+	do 
+		echo Getting task status ...
+		taskstatus=`curl ${srv}/api/ce/task?id=${ceTaskId} | jq -r .task.status`
+		echo "$taskstatus"
+	
+		case "$taskstatus" in
+			"PENDING")
+				echo "Still Pending";
+				sleep 10;
+				continue;
+				;;
+			"IN_PROGRESS")
+				echo "Still In Progress";
+				sleep 10;
+				continue;
+				;;
+			"FAILED")
+				echo "Just Failed";
+				break;
+				;;
+			"CANCELLED")
+				echo "Cancelled";
+				break;
+				;;
+			"SUCCESS")
+				echo "Got success";
+				analysisComplete=true;
+				break;
+				;;
+			"*")
+				echo "Unknown status : $taskstatus";
+				break;
+				;;
+		esac	
+	done
+
+
+	if [ $analysisComplete == false ]; then
+		echo "Analysis has not been completed..."
+		echo "Cannot get quality gate information .."
+	else
+		
+		aID=`curl ${srv}/api/ce/task?id=${ceTaskId} | jq .task.analysisId`
+		
+		# If output of curl is a long string, it gets write failed 23"
+		# on macos. so output to file and read from file"
+		qurlstr="curl ${srv}/api/qualitygates/project_status?analysisId=${aID} -o qgate.out"
+		eval ${qurlstr}
+		qgjson=`cat qgate.out`
+		qgstatus=`echo $qgjson | jq -r .projectStatus.status`
+		
+		echo SONAR_ANLYSIS_ID = $aID
+		echo "Quality Gate Status = $qgstatus"
+		
+		export SONAR_ANALYSIS_ID=${aID};
+		export SONAR_QGATESTATUS=${qgstatus};
+		
+		qgconditions=`echo $qgjson | jq .projectStatus.conditions`
+		clist=`echo $qgconditions | jq -r '.[] | "\(.metricKey) \(.status)"'`
+		echo "All Conditions List"
+		echo $clist
+		# We can also set the conditions list as one env var if need be
+		# but for now, we get each condition metricKey and its value and set
+		# as an environment variable.
+		expallconditions=`echo $qgconditions | jq -r '.[] | "export SONAR_\(.metricKey)=\(.status);"'`
+		
+		echo "Export Command List for all QG Conditions"
+		echo $expallconditions
+
+#
+# If we want to jut set quality gate conditions that have error only, istead of all
+# we can use the following
+#		experrconditions=`echo $qgconditions | jq -r ' .[] | select (.status == "ERROR") | " export SONAR_\(.metricKey)=\(.status);" '`
+#	
+#		echo "Export Command List for ERROR QG Conditions Only"
+#		echo $experrconditions
+# 		echo "Executing Export Command List for ERROR QG Conditions Only"
+#		eval $experrconditions
+#
+		
+		# For now, we want to export all conditions of quality gate status
+		echo "Executing Export Command List for all QG Conditions"
+		eval $expallconditions
+		
+		/bin/rm -f qgate.out
+	
+	fi
+}
+
+
+setReportFile()
+{
+
+	# Approach to find the sonar report to obtain the task id
+	#1. test .sonar/sonar-report.txt
+	#2. else test .scannerwork/sonar-report.txt (SonarScannerCLI)
+	#3. else test target/sonar/sonar-report.txt (Maven)
+	#4. else find sonar-report.txt in all other sub-directories
+
+
+	rptfile=
+	if [ -f .sonar/report-task.txt ]; then
+		rptfile=".sonar/report-task.txt"
+	elif [ -f .scannerwork/report-task.txt ]; then
+		rptfile=".scannerwork/report-task.txt"
+	elif [ -f target/sonar/report-task.txt ]; then
+		rptfile="target/sonar/report-task.txt"
+	else 
+		rptfile=`find . -name report-task.txt -print 2>/dev/null`
+	fi
+}
+
+
+setCiInfo()
+{
+	citype=
+
+	# Based on CITYPE,we can generalize build_name and build_number
+	# In general each ci has its own env vars for build_number.
+	# build_name can be derived from a couple of other vars
+	# like repo_owner-repo_name-branch_id or jobname
+
+	if [ ! -z $CIRCLECI ]; then
+		citype="CIRCLECI"
+		CI_BUILD_NUM=${CIRCLE_BUILD_NUM}
+		CI_BUILD_NAME=${CIRCLE_PROJECT_USERNAME}-${CIRCLE_PROJECT_REPONAME}-${CIRCLE_BRANCH}
+	elif [ ! -z $TRAVIS ]; then
+		citype="TRAVIS"
+		CI_BUILD_NUM=${TRAVIS_BUILD_NUMBER}
+		CI_BUILD_NAME=${TRAVIS_REPO_SLUG}-${TRAVIS_BRANCH}
+	elif [ ! -z $JENKINS_URL ]; then
+		citype="JENKINS"
+		CI_BUILD_NUM=${BUILD_NUMBER}
+		CI_BUILD_NAME=${JOB_NAME}-${GIT_BRANCH}
+	else
+		citype="UNKNOWN"
+		CI_BUILD_NUM=
+		CI_BUILD_NAME=
+	fi
+	echo "Current CI Server is $citype .."
+}
+
+setReportFile;
+
+if [ -z $rptfile ]; then
+	echo "No report file found .. Cannot proceed. Aborting ..."
+	exit 1;
+fi 
+
+echo "Using $rptfile as the report-task file"
+
+setCiInfo;
+echo "Current CI Server is $citype CI_BUILD_NUM = $CI_BUILD_NUM, CI_BUILD_NAME=$CI_BUILD_NAME.."
+
+srv=`grep serverUrl ${rptfile} | awk -F= '{print $2}'`
+ceTaskId=`grep ceTaskId ${rptfile} | awk -F= '{print $2}'`
+echo "Sonar Tasl Id = ${ceTaskId}"
+
+#Set sonar related information in variables prefixed with SONAR_ and export them
+export SONAR_CETASKID=${ceTaskId}
+
+dashboardUrl=`grep dashboardUrl  ${rptfile} | awk -F= '{print $2}'`
+echo "DashboardUrl = $dashboardUrl"
+
+export SONAR_DASHBOARDURL=${dashboardUrl}
+
+taskstatus="PENDING"
+
+# Sonar analysis is async so may not be done immediately
+analysisComplete=false
+
+# If we want to get Quality gate Status then we have to wait
+# for analysis to complete. But this may block for too long.
+# We will do this based on WAIT_FOR_ANALYSIS environment variable
+# but default to true in this version
+
+waitForAnalysis=${WAIT_FOR_ANALYSIS:-true}
+echo "Wait for Analysis is $waitForAnalysis"
+
+# If we want to fail the build based on Quality Gate Status
+# we will do it based on the first argument $1, assumed by default to be true
+# Note: if set to true, but analysis is not complete, then it will be ignored
+#
+failBuildOnQG=${1:-true}
+echo "Fail Build if Quality Gate Status is ERROR = $failBuildOnQG"
+
+if [ "$waitForAnalysis" == "true" ]; then
+	waitForAnalysisToComplete;
+else
+	echo "Not waiting for analysis to complete."
+	echo "We will set taskstatus in the environment variable"
+	echo "Task Id and Task Status should be used later on to get Quality Gate Status"
+fi
+
+export SONAR_TASKSTATUS="$taskstatus";
+
+if [ -z $CI_BUILD_NUM ]; then
+	echo "CI_BUILD_NUM is not configured .."
+	echo "Artifactory Build Info management skipped ..."
+else
+
+	# Get JFrog CLI
+	curl -fL https://getcli.jfrog.io | sh
+
+	# configure artifactory
+	./jfrog rt config jf1  --url $ARTIFACTORY_URL --user $ARTIFACTORY_USER --apikey $ARTIFACTORY_APIKEY --interactive=false
+
+        # Please modify to match the artifacts to be uploaded for your project
+        ./jfrog rt u "*/*.jar" $ARTIFACTORY_REPO --build-name=$CI_BUILD_NAME --build-number=$CI_BUILD_NUM --flat=false --server-id=jf1 
+
+	# read env vars
+	./jfrog rt bce $CI_BUILD_NAME $CI_BUILD_NUM
+
+	# Publish build info
+	./jfrog rt bp $CI_BUILD_NAME $CI_BUILD_NUM --server-id=jf1
+
+fi
+
+
+if [ "$failBuildOnQG" == "true" ]; then
+	echo "We will fail the build if Quality Gate Status has ERROR"
+	echo "QGSTATUS = $qgstatus"
+	if [ "$qgstatus" == "ERROR" ]; then
+		echo "Failing the build as Quality Gate Status has ERROR.."
+		exit 1;
+	else
+		echo "Not Failing the build as Sonar task status is $taskstatus and Quality Gate Status is $qgstatus.."
+	fi
+else
+	echo "Not Failing the build as it is not enabled .."
+fi
+
